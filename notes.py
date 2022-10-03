564b9b66310767dcf9774f098450813c0b54ce35
Arthur <arthur@development.arthurshmidt>
2022-10-03 13:04:43 -0400

    minor progress

--- notes.py
+++ notes.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
 import os
+import sys
 
-print("This is a test")
+NOTES_DIR = "/home/arthur/Documents/notes"
+
+if __name__ == "__main__":
+    try:
+        if sys.argv[1] == "-l":
+            for dirs in os.listdir(NOTES_DIR):
+                print(dirs)
+    except:
+        print("notes -l             // Lists notes")
+        print("notes -e <editfile>  // edits note")
+        print("notes -s             // select note")
