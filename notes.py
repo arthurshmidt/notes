5125812cada2771f83dbc5c91e29f8c079b42fcc
arthurshmidt <arthur@development.arthurshmidt>
2022-10-03 14:59:02 -0400

    better listing

--- notes.py
+++ notes.py
@@ -15,16 +15,23 @@ def print_help():
 if __name__ == "__main__":
     try:
         if len(sys.argv) == 1:
+            counter = 0
             for dirs in os.listdir(NOTES_DIR):
-                print(dirs)
+                counter = counter + 1
+                print(str(counter)+". " + dirs)
         elif sys.argv[1] == "-l":
+            counter = 0
             for dirs in os.listdir(NOTES_DIR):
-                print(dirs)
+                counter = counter + 1
+                print(str(counter) + ". " + dirs)
         elif sys.argv[1] == "-e":
             os.system("vim "+ NOTES_DIR + "/" + sys.argv[2])
         elif sys.argv[1] == "-h":
             print_help()
         elif sys.argv[1] in os.listdir(NOTES_DIR):
             os.system("cat "+ NOTES_DIR + "/" + sys.argv[1])
+        elif sys.argv[1] not in os.listdir(NOTES_DIR):
+            if input("Create file "+sys.argv[1]+".notes (y/n): ") == "y":
+                os.system("vim "+ NOTES_DIR + "/" + sys.argv[1] + ".notes")
     except:
         print_help()
