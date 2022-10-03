1bd1cb20b133f19d831f67aba2b41cfcefb94e93
arthurshmidt <arthur@development.arthurshmidt>
2022-10-03 13:17:50 -0400

    added basic edit

--- notes.py
+++ notes.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
         if sys.argv[1] == "-l":
             for dirs in os.listdir(NOTES_DIR):
                 print(dirs)
+        if sys.argv[1] == "-e":
+            os.system("vim "+ NOTES_DIR + "/" + sys.argv[2])
     except:
         print("notes -l             // Lists notes")
         print("notes -e <editfile>  // edits note")
