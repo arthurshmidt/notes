42f1bf9fbdd0e64108fdb39395bf432229713a82
arthurshmidt <arthur@development.arthurshmidt>
2022-10-03 14:06:13 -0400

    more edits

--- notes.py
+++ notes.py
@@ -5,14 +5,24 @@ import sys
 
 NOTES_DIR = "/home/arthur/Documents/notes"
 
+def print_help():
+    print("notes <no arguments> // list notes")
+    print("notes -l             // Lists notes")
+    print("notes -e <editfile>  // edits note")
+    print("notes -s             // select note")
+    print("notes -h             // list help")
+
 if __name__ == "__main__":
     try:
-        if sys.argv[1] == "-l":
+        if len(sys.argv) == 1:
             for dirs in os.listdir(NOTES_DIR):
                 print(dirs)
-        if sys.argv[1] == "-e":
+        elif sys.argv[1] == "-l":
+            for dirs in os.listdir(NOTES_DIR):
+                print(dirs)
+        elif sys.argv[1] == "-e":
             os.system("vim "+ NOTES_DIR + "/" + sys.argv[2])
+        elif sys.argv[1] == "-h":
+            print_help()
     except:
-        print("notes -l             // Lists notes")
-        print("notes -e <editfile>  // edits note")
-        print("notes -s             // select note")
+        print_help()
